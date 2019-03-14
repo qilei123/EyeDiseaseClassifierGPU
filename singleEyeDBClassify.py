76b7eecf771b4e15f2ecd16637ec96ede74c56af
qilei123 <qileimail123@gmail.com>
2019-03-14 12:37:19 -0400

    fdadf

--- singleEyeDBClassify.py
+++ singleEyeDBClassify.py
@@ -1,6 +1,7 @@
 import os
-
-
+import sys
+sys.path.append("/media/cql/DATA1/Development/opencv/install33/lib")
+sys.path.append("/usr/local/cuda/lib64")
 def classify(img_dir):
     p = os.popen('./CBD '+img_dir)
     x = p.read()
@@ -11,4 +12,4 @@ def main():
     for i in range(10):
         print classify('./Data/921607893.jpg')
 
-main()
+#main()
